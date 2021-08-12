7f6f6682ec7992f8cb762d79537f35e204487c18
Jayden Banks <jayden.banks21@gmail.com>
2021-08-12 12:48:15 -0600

    fixed client folder

--- server/index.js
+++ server/index.js
@@ -2,15 +2,17 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static('client'))
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../html/index.html'))
+    res.sendFile(path.join(__dirname, '../client/index.html'))
 })
-app.get('/styles', (req, res) => {
-    res.sendFile(path.join(__dirname, '../index.css'))
-})
-app.get('/assets', (req, res) => {
-    res.sendFile(path.join(__dirname, '../assets/Bumblebee.jpg'))
-})
+// app.get('/styles', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.css'))
+// })
+// app.get('/assets', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../assets/Bumblebee.jpg'))
+// })
 
 const port = process.env.PORT || 4000
 
